723bb602c338f8f55a0114f25f38423509e28523
mephit <meph.cell@gmail.com>
2023-04-11 20:51:29 +0300

    Tests upgraded

--- test.py
+++ test.py
@@ -1,16 +1,41 @@
+import unittest
 import numpy as np
 from random import randint
+from OPM_start import check_diff_sign, normalize_cc
 
-var1 = [randint(1, 100) for x in range(100)]
 
-positive_cc = [i + 1 for i in var1]
-negative_cc = [i - (2*i) for i in var1]
-no_cc = [randint(1, 100) for i in range(100)]
+class TestNormalizeCC(unittest.TestCase):
+    
+    def test_main(self):
+        self.assertAlmostEqual(normalize_cc(float('nan')), 1.0)
+        self.assertAlmostEqual(normalize_cc(-0.0000001), 0.001)
 
-print(positive_cc)
-print(negative_cc)
-print(no_cc)
 
-print(np.corrcoef(var1, positive_cc)[0,1])
-print(np.corrcoef(var1, negative_cc)[0,1])
-print(np.corrcoef(var1, no_cc)[0,1])
+class TestCheckDiffSign(unittest.TestCase):
+
+    def test_notdiff(self):
+        self.assertFalse(check_diff_sign(-5.1, -5.1))
+        self.assertFalse(check_diff_sign(5.1, 5.0))
+        self.assertFalse(check_diff_sign(5.0, 0.00001))
+        self.assertFalse(check_diff_sign(0.0000001, 0.000001))
+
+    def test_diff(self):
+        self.assertTrue(check_diff_sign(-0.00001, 0.00001))
+        self.assertTrue(check_diff_sign(-5.0, 5.1))
+
+    def test_zeroes(self):
+        self.assertTrue(check_diff_sign(-5.0, 0.0))
+        self.assertTrue(check_diff_sign(0.0, -0.000000001))
+        self.assertTrue(check_diff_sign(5.0, -0.0))
+        self.assertTrue(check_diff_sign(0.0, -0.0))
+
+
+class TestData(unittest.TestCase):
+    
+    prices = [randint(1, 100) for i in range(100)]
+    positive_cc = [i + 1 for i in prices]
+    negative_cc = [i - (2 * i) for i in prices]
+    no_cc = [randint(1, 100) for i in range(100)]
+
+if __name__ == '__main__':
+    unittest.main()
